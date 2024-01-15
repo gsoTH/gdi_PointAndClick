cbea04507652cc8f27cf9eac92fe449ff4f30417
M.Thomas <m.thomas@gso.schule.koeln>
2024-01-15 08:42:19 +0100

    Ausgangslage

---
+++ gdi_PointAndClick/gdi_PointAndClick/FrmMain.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic; // benötigt für Listen
+
+namespace gdi_PointAndClick
+{
+    public partial class FrmMain : Form
+    {
+        List<Rectangle> rectangles = new List<Rectangle>();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            ResizeRedraw = true;
+        }
+
+        private void FrmMain_Paint(object sender, PaintEventArgs e)
+        {
+            // Hilfsvarablen
+            Graphics g = e.Graphics;
+            int w = this.ClientSize.Width;
+            int h = this.ClientSize.Height;
+
+        }
+    }
+}
